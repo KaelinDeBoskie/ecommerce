811744349ed9174233753caebd80c290f36e4061
KaelinDeBoskie <136182895+KaelinDeBoskie@users.noreply.github.com>
2026-02-13 12:45:25 -0700

    feat: add Boltbet page

---
+++ src/app/(pages)/boltbet/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Metadata } from 'next'
+
+export default function BoltbetPage() {
+  return (
+    <section>
+      <h1>Boltbet</h1>
+      <p>Coming soon.</p>
+    </section>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Boltbet',
+  description: 'Boltbet betting experience coming soon.',
+}
